7f3db78c147183d395d4a28aaa354088137fa5bd
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-24 14:13:56 +0600

    header updated

--- src/header/Header.js
+++ src/header/Header.js
@@ -2,8 +2,16 @@ import './header.css';
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className="header">
+            <div className="headerTitles">
+        <span className="headerTitleSm">React & Node</span>
+        <span className="headerTitleLg">BLOG</span>
+      </div>
+      <img
+        className="headerImg"
+        src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        alt=""
+      />
         </div>
     );
 };
