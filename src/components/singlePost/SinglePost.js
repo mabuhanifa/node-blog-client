d6274e1e0e9ac9b5e496b8cba7ad4d3abc659742
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-26 15:10:08 +0600

    updating singlePost

--- src/components/singlePost/SinglePost.js
+++ src/components/singlePost/SinglePost.js
@@ -1,11 +1,22 @@
-import './singlePost.css';
+import "./singlePost.css";
 
 const SinglePost = () => {
-    return (
-        <div className="singlePost">
-            singlepost
+  return (
+    <div className="singlePost">
+      <div className="singlePostWrapper">
+        <img
+          src="https://images.pexels.com/photos/11427593/pexels-photo-11427593.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+          alt="single"
+          className="singlePostImg"
+        />
+        <h1>Lorem ipsum, dolor sit amet ?</h1>
+        <div className="singlePostEdit">
+          <i className="singlePostIcon far fa-edit"></i>
+          <i className="singlePostIcon far fa-trash-alt"></i>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SinglePost;
